a046daa0894c7bc2a43b393d2fb27bb3ab75a0b6
Phil Flanagan <phileflanagan@gmail.com>
2017-01-09 22:50:04 -0500

    add functionality to add and remove comments

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,10 +1,29 @@
-function comments(state = [], action) {
+function postComments(state = [], action) {
   switch(action.type) {
     case 'ADD_COMMENT':
-      console.log('adding comment');
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      return [
+        ...state.slice(0, action.i),
+        ...state.slice(action.i + 1)
+      ];
     default:
       return state;
   }
+}
+
+function comments(state = [], action) {
+  console.log(state);
+  if(typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+  return state;
 }
 
 export default comments
