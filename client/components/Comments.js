216d2242b2bf2d18386a2882709a137f64e8d2ea
Phil Flanagan <phileflanagan@gmail.com>
2017-01-09 21:00:44 -0500

    Introduce single page item

---
+++ client/components/Comments.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Comments extends Component {
+  render() {
+    return (
+      <div className="comment">Testing comment area</div>
+    );
+  }
+}
+
+export default Comments;
--- client/components/photo.js
+++ client/components/photo.js
@@ -18,7 +18,7 @@ class Photo extends Component {
         <figcaption>
           <p>{post.caption}</p>
           <div className="control-buttons">
-            <button className="likes">&hearts; {post.likes}</button>
+            <button onClick={this.props.increment.bind(null, i)} className="likes">&hearts; {post.likes}</button>
             <Link className="button" to={`/views/${post.code}`}>
               <span className="comment-count">
                 <span className="speech-bubble"> </span>
