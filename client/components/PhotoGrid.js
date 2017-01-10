4608f384f99c2ae52e187afff796885fd99e9569
Phil Flanagan <phileflanagan@gmail.com>
2017-01-09 20:12:51 -0500

    display information as cards

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -5,7 +5,7 @@ class PhotoGrid extends Component {
   render() {
     return (
       <div className="photo-grid">
-        {this.props.posts.map((posts, i) => <Photo key={i} i={i} post={post} {...this.props}/>)}
+        {this.props.posts.map((post, i) => <Photo key={i} i={i} post={post} {...this.props}/>)}
       </div>
     );
   }
